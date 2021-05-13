851efc7a271fd3ab3f108ba38368909dd97ba2f3
Diego Rodriguez <dierodz91@gmail.com>
2021-05-13 09:27:28 -0300

    add button component

---
+++ src/components/button.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  padding: 0.25rem 0.75rem;
+  text-transform: uppercase;
+  font-weight: bold;
+`
+
+export default Button;
