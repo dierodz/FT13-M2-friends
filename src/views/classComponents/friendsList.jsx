387493f2fa2bed18c59de0926ba9024a54a54cdf
Diego Rodriguez <dierodz91@gmail.com>
2021-05-13 09:27:40 -0300

    add pages

---
+++ src/views/classComponents/friendsList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FriendsListPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default FriendsListPage
---
+++ src/views/functionalComponents/friendsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FriendsListPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FriendsListPage
